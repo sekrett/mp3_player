62c1c04e81adb439b5ffa1822941b3d7abddbe81
Alexander Zubkov <info@zubkov.info>
2013-06-24 22:41:05 +0400

    Explicitly add all vendor files

--- lib/mp3_player/engine.rb
+++ lib/mp3_player/engine.rb
@@ -2,5 +2,11 @@ module Mp3Player
   # Required for assets to be discoverable in the asset pipeline
   # @private
   class Engine < ::Rails::Engine
+    initializer 'mp3_player.assets_precompile', group: :all do |app|
+          app.config.assets.precompile += [
+              'audio-player.js',
+              'audio-player-noswfobject.js',
+              'player.swf'
+          ]
   end
 end
